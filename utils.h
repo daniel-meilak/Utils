dfd25b51cb4721fd41399068cd6fd541fecf53d4
Daniel Meilak <daniel.mlak@gmail.com>
2022-01-24 22:31:08 +0100

    added string allocator to template template parameters

--- utils.h
+++ utils.h
@@ -243,7 +243,7 @@ std::vector<std::vector<int>> read_int_grid(std::string file_name, bool border =
 
 // convert vector of strings to vector of ints
 template <typename Int = int, template <typename...> typename Cont = std::vector>
-Cont<Int> input_to_int(Cont<std::string> input){
+Cont<Int> input_to_int(Cont<std::string, std::allocator<std::string>> input){
 
    Cont<Int> output;
    Int value;
@@ -269,7 +269,7 @@ Cont<Int> input_to_int(Cont<std::string> input){
 
 // convert 2D vector of vector of strings to vector of vector of ints
 template <typename Int = int, template <typename...> typename Cont2 = std::vector, template <typename...> typename Cont1 = std::vector>
-Cont1<Cont2<Int>> input_to_int_2D(const Cont1<Cont2<std::string>> &input){
+Cont1<Cont2<Int>> input_to_int_2D(const Cont1<Cont2<std::string, std::allocator<std::string>>> &input){
 
    const size_t& size = input.size();
    Cont1<Cont2<Int>> output(size);
