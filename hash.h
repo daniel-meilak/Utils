b3475092ef926a84dad3b8dc37adfb424123eb6a
Daniel Meilak <daniel.meilak@egym.com>
2023-12-21 00:32:06 +0100

    Added template hash for points

--- hash.h
+++ hash.h
@@ -1,12 +1,16 @@
 #pragma once
 
-template <class T>
+template <typename T>
 void hash_combine(std::size_t& seed, const T& v) {
     std::hash<T> hasher;
     seed ^= hasher(v) + 0x9e3779b9 + (seed<<6) + (seed>>2);
 }
 
-template <class Tuple, std::size_t Index = std::tuple_size<Tuple>::value - 1>
+//------------------------------------------------------------------------------
+// Tuple hash
+//------------------------------------------------------------------------------
+
+template <typename Tuple, std::size_t Index = std::tuple_size<Tuple>::value - 1>
 struct HashValueImpl {
     static void apply(size_t& seed, const Tuple& tuple) {
         HashValueImpl<Tuple, Index-1>::apply(seed, tuple);
@@ -14,7 +18,7 @@ struct HashValueImpl {
     }
 };
 
-template <class Tuple>
+template <typename Tuple>
 struct HashValueImpl<Tuple, 0> {
     static void apply(size_t& seed, const Tuple& tuple) {
         hash_combine(seed, std::get<0>(tuple));
@@ -26,6 +30,20 @@ struct TupleHash {
     size_t operator()(const std::tuple<TT...>& tt) const {
         size_t seed = 0;
         HashValueImpl<std::tuple<TT...>>::apply(seed, tt);
+        return seed;
+    }
+};
+
+//------------------------------------------------------------------------------
+// Pair hash
+//------------------------------------------------------------------------------
+
+template <typename T1, typename T2>
+struct PairHash {
+    size_t operator()(const std::pair<T1, T2>& p) const {
+        size_t seed = 0;
+        hash_combine(seed, p.first);
+        hash_combine(seed, p.second);
         return seed;
     }
 };
